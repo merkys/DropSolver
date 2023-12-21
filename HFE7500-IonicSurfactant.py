d887a76e1bf3f29057f5de4d435706fd0e26e941
Andrius Merkys <andrius.merkys@gmail.com>
2023-12-21 14:10:34 +0200

    Start by converting the first code block.

---
+++ HFE7500-IonicSurfactant.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+Kd = 0.001      # water viscosity, [Pa*s] *)
+Kvisc = 0.0014  # oil consistency index, [Pa*s^n] *)
+EtaZero = 0.0014
+EtaInf = 0.0014
+wn = 7 * 10 ** -5   # width of the focussing nozzle, [m] *)(* 20/20/H11; 28x15.5/H17; 33/5/H18; 37.5/4.5/20; 10x17; 33.5x5//17; 37x4//H20*)
+Ln = 7 * 10 ** -5   # length of the focussing nozzle, [m] *)
+H = 8 * 10 ** -5    # Height of the chip, [m] *)
+sigmaEQ = (52) * 0.001  # surface tension of water/oil interface with no surfactant *)
+wcont = 6 * 10 ** -5    # Width of the Continuous-phase channel *)
+wdisp = 7 * 10 ** -5    # Width of the Dispersed-phase channel *)
+wout = 11 * 10 ** -5    # Width of the outlet channel *)
+RhoO = 1614     # Oil density [kg/m^3] *)
+RhoW = 1000     # Water density, [kg/m^3] *)
+Ms = 7.5    # Molar mass of surfactant [kg/mol] *)
+omega = 0.006   # surfactant fraction, USED IN OIL FOR EXPERIMENT, w/w *)
+NA = 6.023 * 10 ** 23 # Avogadro Number *)
+R = 8.314   # Universal gas-constant *)
+T = 293     # Absolute temperature, [K] *)
+GAMMAinf = 8 * 10 ** -6 # Surface excess of surfactant at complete-coverage of interface [mol/m^2] *)
+Kads = 1400
+Kdes = 0.0002
+CMC = 0.128     # Critical Micelle Concentration, [mol/m^3] *)
+n = 1.0     # NONLINEARITY INDEX for continuous phase*)
+p = 1.0     # NONLINEARITY INDEX for dispersed phase*)
+EpsilonHFE = 5.8
+F = 96400
+z = 1
+r0 = 0.2 * 10 ** -9 * 45 ** 0.6
