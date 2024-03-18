faed44259b60b6b69d6f171118218c6e5e5eccf7
Andrius Merkys <andrius.merkys@gmail.com>
2024-03-18 15:32:15 +0200

    Some more parameters.

--- shiny/app.py
+++ shiny/app.py
@@ -17,12 +17,14 @@ app_ui = ui.page_fluid(
     # CHIP GEOMETRIC PARAMETERS
     ui.input_numeric("wn", "Nozzle width (wn) [μm]", 70, min=5, max=200), # * 10 ** -6
     ui.input_numeric("Ln", "Nozzle length (Ln), [μm]", 70, min=5, max=200),
-    ui.input_numeric("H", "Channel height [μm]", 70, min=5, max=200), # FIXME - what is the default?
+    ui.input_numeric("H", "Channel height [μm]", 80, min=5, max=200),
     ui.input_numeric("wcont", "Width of the Continuous-phase channel (wcont) [μm]", 60, min=5, max=300),
     ui.input_numeric("wdisp", "Width of the Dispersed-phase channel (wdisp) [μm]", 70, min=5, max=300),
     ui.input_numeric("wout", "Width of the outlet channel (wout) [μm]", 110, min=5, max=300),
 
     ui.input_numeric("omega", "Fraction of dissolved surfactant", 0.006, min=0, max=1, step=0.001),
+    ui.input_numeric("sigmaEQ", "Interface tension without any surfactant [mN/m]", 52, min=0.1, max=72), # * 0.001
+    ui.input_numeric("Qw", "Dispersed-phase flow rate [μl/hr]", 220 * 2.78 * 10 ** -13), # FIXME: Is this OK?
     ui.input_switch("is_ionic", "Ionic", True),
     ui.input_action_button("calculate", "Calculate"),
     ui.output_data_frame("result_dataframe"),
@@ -43,6 +45,6 @@ def server(input: Inputs, output: Outputs, session: Session):
     @reactive.event(input.calculate)
     def _():
         table = calculate(is_ionic=input.is_ionic(), omega=input.omega())
-        df.set(DataFrame(table, columns=["Qoil", "Tdrop"]))
+        df.set(DataFrame(table, columns=["Qoil [μl/hr]", "Tdrop [pl]"]))
 
 app = App(app_ui, server)
