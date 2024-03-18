e5b307a91b15d8dcaa605d7876bd69511edfab27
Andrius Merkys <andrius.merkys@gmail.com>
2024-03-18 18:23:03 +0200

    Generating the input parameters for the app automatically.

--- shiny/app.py
+++ shiny/app.py
@@ -5,31 +5,16 @@ import numpy
 import pandas
 import surfactant.parameters
 
+def create_numeric_input(parameter):
+    if 'dimension' in parameter:
+        parameter['description'] += ' [{}]'.format(parameter['dimension'])
+    return ui.input_numeric(parameter['parameter'], parameter['description'], parameter['default_value'])
+
+numeric_inputs = [create_numeric_input(p) for p in filter(lambda p: 'default_value' in p, surfactant.parameters.parameters())]
+
 app_ui = ui.page_fluid(
     ui.output_image("junction"),
-
-    # NON-NEWTONIAN VISCOSITY PARAMETERS
-    ui.input_numeric("Kd", "Zero-shear viscosity (disperse phase) [Pa*s]", 0.001, min=0.0005, max=5),
-    ui.input_numeric("etaINF1", "Infinite-shear viscosity (disperse phase) [Pa*s]", 0.001, min=0.0002, max=0.05),
-    ui.input_numeric("B1", "Consistency (Cross time constant), (disperse phase) [s]", 4.691, min=0.1, max=2),
-    ui.input_numeric("p", "Cross rate constant (disperse phase)", 1.0, min=0.1, max=2),
-    ui.input_numeric("Kvisc", "Consistency index (continuous phase) [Pa*s]", 0.0014),
-    ui.input_numeric("EtaZero", "Zero-shear viscosity (continuous phase) [Pa*s]", 0.0014, min=0.0005, max=5),
-    ui.input_numeric("EtaInf", "Infinite-shear viscosity (continuous phase) [Pa*s]", 0.0014, min=0.0002, max=0.05),
-    ui.input_numeric("B2", "Consistency (Cross time constant), (continuous phase) [s]", 77, min=0.01, max=100),
-    ui.input_numeric("n", "Cross rate constant (disperse phase)", 1, min=0.1, max=2),
-
-    # CHIP GEOMETRIC PARAMETERS
-    ui.input_numeric("wn", "Nozzle width (wn) [μm]", 70, min=5, max=200), # * 10 ** -6
-    ui.input_numeric("Ln", "Nozzle length (Ln), [μm]", 70, min=5, max=200),
-    ui.input_numeric("H", "Channel height [μm]", 80, min=5, max=200),
-    ui.input_numeric("wcont", "Width of the Continuous-phase channel (wcont) [μm]", 60, min=5, max=300),
-    ui.input_numeric("wdisp", "Width of the Dispersed-phase channel (wdisp) [μm]", 70, min=5, max=300),
-    ui.input_numeric("wout", "Width of the outlet channel (wout) [μm]", 110, min=5, max=300),
-
-    ui.input_numeric("omega", "Fraction of dissolved surfactant", 0.006, min=0, max=1, step=0.001),
-    ui.input_numeric("sigmaEQ", "Interface tension without any surfactant [mN/m]", 52, min=0.1, max=72), # * 0.001
-    ui.input_numeric("Qw", "Dispersed-phase flow rate [μl/hr]", 220 * 2.78 * 10 ** -13, min=10, max=10000), # FIXME: Is this OK?
+    *numeric_inputs,
     ui.input_switch("is_ionic", "Ionic", True),
     ui.input_action_button("calculate", "Calculate"),
     ui.output_data_frame("result_dataframe"),
