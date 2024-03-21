fa4b0da241571cc309f7f937d7c935518a0f44cd
Andrius Merkys <andrius.merkys@gmail.com>
2024-03-21 15:25:06 +0200

    Add all the parameters from Valdemaras's new test case.

--- tests/test_surfactant.py
+++ tests/test_surfactant.py
@@ -3,6 +3,7 @@ import numpy
 import unittest
 
 Q_SI_multiplier = 2.78 * 10 ** -13
+micrometre = 10e-6
 
 class TestStringMethods(unittest.TestCase):
 
@@ -14,5 +15,5 @@ class TestStringMethods(unittest.TestCase):
 
     def test_newtonian_ionic_0_6_perc(self):
         target = numpy.array([[50, 385.954], [100, 269.174], [150, 213.79], [200, 179.768], [250, 156.414], [300, 139.246]])
-        result = calculate(is_ionic=True, Qw=50*Q_SI_multiplier, QoilStart=50*Q_SI_multiplier, QoilEnd=300*Q_SI_multiplier, QoilStep=50*Q_SI_multiplier)
+        result = calculate(is_ionic=True, Qw=50*Q_SI_multiplier, QoilStart=50*Q_SI_multiplier, QoilEnd=300*Q_SI_multiplier, QoilStep=50*Q_SI_multiplier, B1=1, B2=1, wn=37*micrometre, Ln=13*micrometre, H=18*micrometre, wcont=120*micrometre, wdisp=80*micrometre, wout=70*micrometre)
         self.assertLess(abs((target - result).min()), 20)
