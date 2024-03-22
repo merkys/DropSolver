badb7300f946d6b1df4c07ff86954673bff181e3
Andrius Merkys <andrius.merkys@gmail.com>
2024-03-22 08:45:45 +0200

    Adding one more test case.

--- tests/test_surfactant.py
+++ tests/test_surfactant.py
@@ -25,3 +25,9 @@ class TestStringMethods(unittest.TestCase):
         target = numpy.array([[50, 385.955], [100, 269.174], [150, 213.79], [200, 179.768], [250, 156.414], [300.,139.246]])
         result = calculate(is_ionic=False, Qw=50*Q_SI_multiplier, QoilStart=50*Q_SI_multiplier, QoilEnd=300*Q_SI_multiplier, QoilStep=50*Q_SI_multiplier, B1=1, B2=1, wn=37*micrometre, Ln=13*micrometre, H=18*micrometre, wcont=120*micrometre, wdisp=80*micrometre, wout=70*micrometre)
         self.assertLess(abs((target - result).min()), 20)
+
+    # From 'notebooks/2024-03-21/HFE7500+ non-Newtonian disp.phase, NonionicSurfactant (0.6perc).txt'
+    def test_non_test_newtonian_nonionic_0_6_perc(self):
+        target = numpy.array([[50, -145127], [100, -1151.19], [150, 927.812], [200, 485.574], [250, 340.443], [300.,265.617]])
+        result = calculate(is_ionic=False, Kd=0.381, etaINF1=0.00375, B1=4.691, B2=1, p=0.529, wn=37*micrometre, Ln=13*micrometre, H=18*micrometre, wcont=120*micrometre, wdisp=80*micrometre, wout=70*micrometre)
+        self.assertLess(abs((target - result).min()), 20)
