10169e92cb52f75936ec10ef243aafdb8e982b47
Andrius Merkys <andrius.merkys@gmail.com>
2024-03-25 15:48:15 +0200

    Updating one more test case.

--- tests/test_surfactant.py
+++ tests/test_surfactant.py
@@ -20,10 +20,10 @@ class TestStringMethods(unittest.TestCase):
         result = calculate(is_ionic=True, Qw=50*Q_SI_multiplier, QoilStart=50*Q_SI_multiplier, QoilEnd=300*Q_SI_multiplier, QoilStep=50*Q_SI_multiplier, B1=1, B2=1, wn=37*micrometre, Ln=13*micrometre, H=18*micrometre, wcont=120*micrometre, wdisp=80*micrometre, wout=70*micrometre)
         self.assertLess(abs((target - result).min()), 20)
 
-    # From 'notebooks/2024-03-21/Newtonian HFE7500- NonionicSurfactant (0.6perc).txt'
+    # From 'notebooks/2024-03-22/HFE7500-NonionicSurfactant (0.6perc) [2].txt'
     def test_newtonian_nonionic_0_6_perc(self):
-        target = numpy.array([[50, 385.955], [100, 269.174], [150, 213.79], [200, 179.768], [250, 156.414], [300.,139.246]])
-        result = calculate(is_ionic=False, Qw=50*Q_SI_multiplier, QoilStart=50*Q_SI_multiplier, QoilEnd=300*Q_SI_multiplier, QoilStep=50*Q_SI_multiplier, B1=1, B2=1, wn=37*micrometre, Ln=13*micrometre, H=18*micrometre, wcont=120*micrometre, wdisp=80*micrometre, wout=70*micrometre)
+        target = numpy.array([[50, 107.429], [100, 76.506], [150, 59.4823], [200, 48.9298], [250, 41.8043], [300, 36.6827]])
+        result = calculate(is_ionic=False, B2=1, Qw=50*Q_SI_multiplier, QoilStart=50*Q_SI_multiplier, QoilEnd=301*Q_SI_multiplier, QoilStep=50*Q_SI_multiplier, wn=37*micrometre, Ln=13*micrometre, H=18*micrometre, wcont=120*micrometre, wdisp=80*micrometre, wout=70*micrometre)
         self.assertLess(abs((target - result).min()), 20)
 
     # From 'notebooks/2024-03-22/HFE7500-NonionicSurfactant (0.6perc) non-Newtonian disp.phase [2].txt'
