4e411307e6c5633bd2c9dc5b2067d8e38f99bf69
Andrius Merkys <andrius.merkys@gmail.com>
2024-11-28 10:44:20 +0200

    Fix a test name.

--- tests/test_surfactant.py
+++ tests/test_surfactant.py
@@ -20,7 +20,7 @@ class TestSurfactant(unittest.TestCase):
         self.assertLess(max(abs(target[:,1] - result[:,1])/target[:,1]), 0.1)
 
     # From 'notebooks/2024-03-22/HFE7500-NonionicSurfactant (0.6perc) non-Newtonian disp.phase [2].txt'
-    def test_non_test_newtonian_nonionic_0_6_perc(self):
+    def test_non_newtonian_nonionic_0_6_perc(self):
         target = numpy.array([[50, 1146.36], [100, 290.37], [150, 182.068], [200, 136.282], [250, 110.076], [300, 92.742]])
         result = calculate(is_ionic=False, Kd=0.381, etaINF1=0.00375, B1=4.691, B2=1, p=0.529, wn=37*micrometre, Ln=13*micrometre, H=18*micrometre, wcont=120*micrometre, wdisp=80*micrometre, wout=70*micrometre, Qw=50*Q_SI_multiplier, QoilStart=50*Q_SI_multiplier, QoilEnd=300*Q_SI_multiplier, QoilStep=50*Q_SI_multiplier)
         self.assertLess(max(abs(target[:,1] - result[:,1])/target[:,1]), 0.1)
