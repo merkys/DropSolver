55f1e9f5ee67b43659344e627f928b8f1be307cc
Andrius Merkys <andrius.merkys@gmail.com>
2024-11-28 10:56:34 +0200

    Add two more tests.

--- tests/test_surfactant.py
+++ tests/test_surfactant.py
@@ -24,3 +24,15 @@ class TestSurfactant(unittest.TestCase):
         target = numpy.array([[50, 1146.36], [100, 290.37], [150, 182.068], [200, 136.282], [250, 110.076], [300, 92.742]])
         result = calculate(is_ionic=False, Kd=0.381, etaINF1=0.00375, B1=4.691, B2=1, p=0.529, wn=37*micrometre, Ln=13*micrometre, H=18*micrometre, wcont=120*micrometre, wdisp=80*micrometre, wout=70*micrometre, Qw=50*Q_SI_multiplier, QoilStart=50*Q_SI_multiplier, QoilEnd=300*Q_SI_multiplier, QoilStep=50*Q_SI_multiplier)
         self.assertLess(max(abs(target[:,1] - result[:,1])/target[:,1]), 0.1)
+
+    # From 'notebooks/2024-11-27/Ionic Surfactant 6perc. DefaultParameters.txt'
+    def test_newtonian_ionic_6_perc(self):
+        target = numpy.array([[90, 3803.16], [190, 3709.73], [290, 3386.78], [390, 3053.68], [490, 2760.52], [590, 2511.11]])
+        result = calculate(is_ionic=True, omega=0.06)
+        self.assertLess(max(abs(target[:,1] - result[:,1])/target[:,1]), 0.1)
+
+    # From 'notebooks/2024-11-27/NonIonic Surfactant 6perc. DefaultParameters.txt'
+    def test_newtonian_ionic_6_perc(self):
+        target = numpy.array([[90, 4676.24], [190, 3968.09], [290, 3485.59], [390, 3097.56], [490, 2782.29], [590, 2522.87]])
+        result = calculate(is_ionic=False, omega=0.06)
+        self.assertLess(max(abs(target[:,1] - result[:,1])/target[:,1]), 0.1)
